b73b00b2a533a986a9b66702a95598617535e32d
RaphaelWright <raphaelwright2003@gmail.com>
2024-10-15 11:42:58 +0000

    updates

---
+++ frontend/rmbp-frontend/src/pages/Settings.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+const Settings = () => {
+    return (
+        <div id="chat-page" className="flex flex-col h-screen bg-gradient-to-b from-teal-700 to-teal-900 text-white">
+        <Header />
+        <main>
+            <h1>Settings</h1>
+        </main>
+      </div>
+    )
+  }
+  
+  export default Settings

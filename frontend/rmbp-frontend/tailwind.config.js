227da2000a7cf51e1464a9487688c68a6059a264
IT Compliance <compliance@amalitech.org>
2024-10-15 04:28:48 -0700

    updates

--- frontend/rmbp-frontend/tailwind.config.js
+++ frontend/rmbp-frontend/tailwind.config.js
@@ -5,23 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-<<<<<<< HEAD
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
       }
     },
-=======
-    colors: {
-      background : '#3c3c3c',
-      fonts: '#ffecc8',
-      olive: '#004a4f',
-      lightgreen: '#00ff44',
-      red: '#ff6666',
-
-    },
-    extend: {},
->>>>>>> dfde2d2f9c7b4a2066849b79435b2c0ead4318d4
   },
   plugins: [],
 }

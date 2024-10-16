59d81bc344610acd7c953580da04f687697d7391
IT Compliance <compliance@amalitech.org>
2024-10-16 06:40:04 -0700

    404 page added

--- frontend/rmbp-frontend/src/pages/Notfound.jsx
+++ frontend/rmbp-frontend/src/pages/Notfound.jsx
@@ -1,8 +1,15 @@
+import errorImage from '../assets/404-error.png'
 
-const Notfound = () => {
+const NotFound = () => {
   return (
-    <div>Notfound</div>
-  )
-}
+    <div className="flex flex-col justify-center items-center h-screen bg-olive text-center p-4">
+      <img 
+        src={errorImage} 
+        alt="404 Error" 
+        className="w-[80%] max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl"
+      />
+    </div>
+  );
+};
 
-export default Notfound
+export default NotFound;

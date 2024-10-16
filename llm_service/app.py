3a1cea01787554a6666173cf7db4e488c3e081ce
Bernard Ograh <bernard.ograh@amalitechtraining.org>
2024-10-16 09:06:08 +0000

    change directory name

---
+++ llm_service/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Api
+from routes import swagger
+from routes.chat import Chat
+from routes.translate import Translate
+
+app = Flask(__name__)
+api = Api(app)
+
+app.register_blueprint(swagger.swagger_ui_blueprint, url_prefix=swagger.SWAGGER_URL)
+
+api.add_resource(Chat, '/api/v1/chat')
+api.add_resource(Translate, '/api/v1/translate')
+
+if __name__ == "__main__":
+    app.run(debug=True)
